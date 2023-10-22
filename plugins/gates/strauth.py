c0db076f111df85cb63c65d733af3f2f75272cc5
DARKWEB REBEL <78189163+DARKWEBREBEL@users.noreply.github.com>
2023-10-21 23:05:40 -1000

    Update strauth.py

--- plugins/gates/strauth.py
+++ plugins/gates/strauth.py
@@ -11,7 +11,7 @@ import json
 session = requests.session()
 
 
-@Client.on_message(filters.command('sa'))
+@Client.on_message(filters.command(''))
 async def cmd_au(Client, message):
     try:
         # NES TOOLS
