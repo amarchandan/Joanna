f1a2bee140d321f6c40114c96d7d060d2c74f7c4
DARKWEB REBEL <78189163+DARKWEBAMAR@users.noreply.github.com>
2023-12-05 21:12:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+import asyncio
+import uvicorn
+import os
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse, StreamingResponse
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    HOST = os.getenv("HOST", "0.0.0.0")
+    PORT = os.getenv("PORT", 8080)
+    uvicorn.run(app, host=HOST, port=PORT)
