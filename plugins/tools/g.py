eb430e41c3989b7463c190fbb0af27cfb1a46ccd
DARKWEB REBEL <78189163+DARKWEBAMAR@users.noreply.github.com>
2023-12-14 21:30:13 +0530

    Update g.py

--- plugins/tools/g.py
+++ plugins/tools/g.py
@@ -225,8 +225,7 @@ async def generate_cc(_, message: Message):
 
             quantity = int(quantity[0]) if quantity else 20
             tic = time.perf_counter()
-	    chkst = "Generating ...."
-	    done = await message.reply_text(chkst, message.id)
+	    done = await message.reply_text('Generating ....', message.id)
             session = requests.session()
             bin = session.get(f"https://lookup.binlist.net/{bin_input}").json()
             try:
