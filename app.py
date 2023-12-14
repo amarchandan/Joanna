65bc05ccddcb70e79f56633eb77c3d2c5b5c6697
DARKWEB REBEL <78189163+DARKWEBAMAR@users.noreply.github.com>
2023-12-14 20:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'LAMO'
+# @app.route('/')
+# def hello_world():
+#     return 'LAMO'
 
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
+
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
