0c9d4dd0e1716c549b58ce8f19f4ba7d615860b3
DARKWEB REBEL <78189163+DARKWEBAMAR@users.noreply.github.com>
2023-12-17 12:39:00 +0530

    Add files via upload

--- plugins/helper/ss.py
+++ plugins/helper/ss.py
@@ -2,6 +2,9 @@ from pyrogram import Client, filters
 from plugins.func.users_sql import *
 from plugins.helper.inline import *
 
+
+grp = "-1002122350640"
+
 @Client.on_message(filters.command ('start'))
 async def cmd_start(Client,message):
   try:
@@ -15,18 +18,19 @@ async def cmd_start(Client,message):
           user_id = str(message.from_user.id)
           chat_type = str(message.chat.type)
           chat_id = str(message.chat.id)
-          text = """
+          text = f"""
 Welcome to Beta version 0.2 of Joanna! >_
 
-Stay Updated! Subscribe To Our Announcement Channel @TeamMorpho For The Latest News!
+{len(getalldata())} Users Were Active In The Last 24 Hours.
 
-This Is The CC Checker Bot With Many Gates And Tools.\n
-This Is The Most Optimized Version And Adapted To Multiple Tasks.\n
+This Is The CC Checker Bot With Many Gates And Tools.
+This Is The Most Optimized Version And Adapted To Multiple Tasks.
 Maybe you already know this bot, Click On Menu to know all my gates.
 """
           edit = await message.reply(
               text=text,
               reply_markup=InlineKeyboardMarkup(menu)
               )
+          await Client.send_message(grp,f"NEW USERS {user_id}")
   except Exception as e:
       print(e)
