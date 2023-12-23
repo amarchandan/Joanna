b4db432040bcbd520fa15eefd9e20b113736bd62
DARKWEB REBEL <78189163+DARKWEBAMAR@users.noreply.github.com>
2023-12-23 12:05:47 +0530

    Update domaintoip.py

--- plugins/tools/domaintoip.py
+++ plugins/tools/domaintoip.py
@@ -30,54 +30,34 @@ async def cmd_domip(Client, message):
             pm = fetchinfo(user_id)
             status = pm[2]
             role = status
-            if chat_type == "ChatType.PRIVATE" and status == "FREE":
-                resp = "⚠️ #PREMIUM_ONLY ⚠️ \n Contact @K3VIN_X To Buy Premium Access !.Else You Can Use Free Then Join @MorPhoChat !"
-                await message.reply_text(resp, message.id)
-            else:
-                
-                if not message.reply_to_message:
-                    return await message.reply_text("Please Reply To File")
-                if not message.reply_to_message.document:
-                    return await message.reply_text("Please Reply To File")
-                tic = time.perf_counter()
-                ms_ = 'Checking....'
-                domain_file = await message.reply_to_message.download(progress_args=(ms_, f"`Downloading This File!`"))
-                with open(domain_file, 'r') as file:
-                    domains = file.read().splitlines()
-                x = len(domains)
-                if status == 'FREE' and x > 1000:
-                    resp = f"""
-#ALERT_ ⚠️
-Your Account Is FREE
-You Can't Check More Than 1000 Domain!
-Upgrade Your Plan Or Wait For Next Update!
-                """
-                    await message.reply_text(resp, message.id)
-                elif status == 'PREMIUM' and x > 50000:
-                    resp = f"""
-#ALERT_ ⚠️
-Your Account Is PREMIUM But You Have Check More Than 50000 SK!
-                """
-                    await message.reply_text(resp, message.id)
-                else:
-                    await message.reply_text(ms_)
-                    results = []
-                    for url in domains:
-                        if 'http://' not in url:
-                            IP1 = socket.gethostbyname(url)
-                            results.append(IP1)
-                        elif 'http://' in url:
-                            url = url.replace('http://', '').replace('https://', '').replace('/', '')
-                            IP2 = socket.gethostbyname(url)
-                            results.append(IP2)
+            if not message.reply_to_message:
+                return await message.reply_text("Please Reply To File")
+            if not message.reply_to_message.document:
+                return await message.reply_text("Please Reply To File")
+            tic = time.perf_counter()
+            ms_ = 'Checking....'
+            domain_file = await message.reply_to_message.download(progress_args=(ms_, f"`Downloading This File!`"))
+            with open(domain_file, 'r') as file:
+                domains = file.read().splitlines()
+            x = len(domains)
+            await message.reply_text(ms_)
+            results = []
+            for url in domains:
+                if 'http://' not in url:
+                    IP1 = socket.gethostbyname(url)
+                    results.append(IP1)
+                elif 'http://' in url:
+                    url = url.replace('http://', '').replace('https://', '').replace('/', '')
+                    IP2 = socket.gethostbyname(url)
+                    results.append(IP2)
 
-                    if results:
-                        result_text = "\n".join(results)
-                        with open(f"{x}x_DOMAIN_TO_IP_BY_@JoannaChkBot.txt", 'w') as result_file:
-                            result_file.write(result_text)
-                    toc = time.perf_counter()
-                    chat_id = str(message.chat.id)
-                    resp=f"""
+            if results:
+                result_text = "\n".join(results)
+                with open(f"{x}x_DOMAIN_TO_IP_BY_@JoannaChkBot.txt", 'w') as result_file:
+                    result_file.write(result_text)
+            toc = time.perf_counter()
+            chat_id = str(message.chat.id)
+            resp=f"""
  COMPLETED ✅
 
 ┏－－－－－－－－－－－－┒
@@ -86,10 +66,10 @@ Your Account Is PREMIUM But You Have Check More Than 50000 SK!
 ┠ Con By - <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> [ {role} ]
 ┠ 𝘋𝘦𝘝 - <a href="tg://user?id=1418571871">̠K̠̠E̠̠V̠̠I̠̠N̠ ̠X̠ ⚠️</a>
 ┗－－－－－－－－－－－－┛"""
-                    await message.reply_document(
-                    document=f"{x}x_DOMAIN_TO_IP_BY_@JoannaChkBot.txt",
-                    caption=resp,
-                    reply_to_message_id=message.id)
-                    os.remove(f"{x}x_DOMAIN_TO_IP_BY_@JoannaChkBot.txt")
+            await message.reply_document(
+            document=f"{x}x_DOMAIN_TO_IP_BY_@JoannaChkBot.txt",
+            caption=resp,
+            reply_to_message_id=message.id)
+            os.remove(f"{x}x_DOMAIN_TO_IP_BY_@JoannaChkBot.txt")
     except Exception as e:
         print(e)
