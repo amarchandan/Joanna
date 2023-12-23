4783e417a1caa300e943790d637b6fb95c29ebd0
DARKWEB REBEL <78189163+DARKWEBAMAR@users.noreply.github.com>
2023-12-23 12:16:21 +0530

    Update skgen.py

--- plugins/tools/skgen.py
+++ plugins/tools/skgen.py
@@ -33,70 +33,50 @@ async def cmd_skgen(Client, message):
             pm = fetchinfo(user_id)
             status = pm[2]
             role = status
-            if chat_type == "ChatType.PRIVATE" and status == "FREE":
-                resp = "⚠️ #PREMIUM_ONLY ⚠️ \n Contact @K3VIN_X To Buy Premium Access !.Else You Can Use Free Then Join @MorPhoChat !"
-                await message.reply_text(resp, message.id)
-                ipn = message.text[len('/gensk '):]
+            if message.reply_to_message:
+                sknumxx = message.reply_to_message.text
             else:
-                if message.reply_to_message:
-                    sknumxx = message.reply_to_message.text
-                else:
-                    sknumxx = message.text[len('/gensk '):]
-                    sknumxx1 = message.text[len('/gensk  '):]
-                if len(sknumxx) == 0:
-                    nocc = """
+                sknumxx = message.text[len('/gensk '):]
+                sknumxx1 = message.text[len('/gensk  '):]
+            if len(sknumxx) == 0:
+                nocc = """
 OPPS! WRONG FORMAT
 
 USE :- /gensk 200 1
           """
-                    return await message.reply_text(nocc, message.id)
-                elif len(sknumxx1) == 1:
-                    nocc = """
+                return await message.reply_text(nocc, message.id)
+            elif len(sknumxx1) == 1:
+                nocc = """
 OPPS! WRONG FORMAT
 
 USE :- /gensk 200 1
           """
-                    return await message.reply_text(nocc, message.id)
+                return await message.reply_text(nocc, message.id)
 
-                ress = "Generating..."
-                tic = time.perf_counter()
-                msg = message.text[len('/gensk '):]
-                splitter = msg.split(' ')
-                num = int(splitter[0])
-                options = splitter[1]
-                if status == 'FREE' and num > 1000:
-                    resp = f"""
-#ALERT_ ⚠️
-Your Account Is FREE
-You Can't Gen More Than 500 SK!
-Upgrade Your Plan Or Wait For Next Update!
-                """
-                    await message.reply_text(resp, message.id)
-                elif status == 'PREMIUM' and num > 10000:
-                    resp = f"""
-#ALERT_ ⚠️
-Your Account Is PREMIUM But You Have Check More Than 10k SK!
-                """
-                    await message.reply_text(resp, message.id)
-                else:
-                    sk_list = []
-                    firstchk = await message.reply_text(ress, message.id)
-                    for _ in range(num):
-                        option = random.choice(options)
-                        if option == '1':
-                            length = 64
-                        elif option == '2':
-                            length = 34
-                        elif option == '3':
-                            length = 24
-                        random_string = generate_random_string(length)
-                        skk = "sk_live_" + random_string
-                        sk_list.append(skk)
-                    with open(f"{num}x_SK_GEN_BY_@JoannaChkBot.txt", "w") as file:
-                        file.write('\n'.join(sk_list))
-                    toc = time.perf_counter()
-                    chat_id = str(message.chat.id)
-                    resp=f"""
+            ress = "Generating..."
+            tic = time.perf_counter()
+            msg = message.text[len('/gensk '):]
+            splitter = msg.split(' ')
+            num = int(splitter[0])
+            options = splitter[1]
+            sk_list = []
+            firstchk = await message.reply_text(ress, message.id)
+            for _ in range(num):
+                option = random.choice(options)
+                if option == '1':
+                    length = 64
+                elif option == '2':
+                    length = 34
+                elif option == '3':
+                    length = 24
+                random_string = generate_random_string(length)
+                skk = "sk_live_" + random_string
+                sk_list.append(skk)
+            with open(f"{num}x_SK_GEN_BY_@JoannaChkBot.txt", "w") as file:
+                file.write('\n'.join(sk_list))
+            toc = time.perf_counter()
+            chat_id = str(message.chat.id)
+            resp=f"""
 GENERATED COMPLETED ✅
 
 ┏－－－－－－－－－－－－┒
@@ -105,10 +85,12 @@ GENERATED COMPLETED ✅
 ┠ Gen By -  <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> [ {role} ]
 ┠ 𝘋𝘦𝘝 - <a href="tg://user?id=1418571871">̠K̠̠E̠̠V̠̠I̠̠N̠ ̠X̠ ⚠️</a>
 ┗－－－－－－－－－－－－┛"""
-                    await message.reply_document(
-                    document=f"{num}x_SK_GEN_BY_@JoannaChkBot.txt",
-                    caption=resp,
-                    reply_to_message_id=message.id)
-                    os.remove(f"{num}x_SK_GEN_BY_@JoannaChkBot.txt")
+            await message.reply_document(
+            document=f"{num}x_SK_GEN_BY_@JoannaChkBot.txt",
+            caption=resp,
+            reply_to_message_id=message.id)
+            os.remove(f"{num}x_SK_GEN_BY_@JoannaChkBot.txt")
     except Exception as e:
         print(e)
+
+
