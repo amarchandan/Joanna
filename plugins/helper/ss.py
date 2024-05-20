5ba49694650b6fc20029bf0a0c176dca0ac56765
AMAR CHANDAN <78189163+amarchandan@users.noreply.github.com>
2024-05-20 14:40:53 +0530

    Update ss.py

--- plugins/helper/ss.py
+++ plugins/helper/ss.py
@@ -1,12 +1,34 @@
 from pyrogram import Client, filters
-
+import time
+from datetime import date
+from pyrogram import Client, filters
 from plugins.func.users_sql import *
 from plugins.helper.inline import *
 
+log = -1001582458495
 
 @Client.on_message(filters.command(["start", "cmd", "cmds", "help"]))
 async def cmd_start(Client, message):
     try:
+        user_id = str(message.from_user.id)
+        username = str(message.from_user.username)
+        str(message.chat.id)
+        antispam_time = int(time.time())
+        reg_at = str(date.today())
+        regdata = fetchinfo(user_id)
+        results = str(regdata)
+        bot_id = f"JOANNA-{gcgenfunc()}"
+        if results == "None":
+            insert_reg_data(user_id, username, antispam_time, reg_at, bot_id)
+            pm = fetchinfo(user_id)
+            role = pm[2]
+            botid = pm[10]
+            credit = pm[5]
+            plan = pm[3]
+            aniti = pm[6]
+            await message.reply_text(log, "new user")
+        else:
+            None
         user_id = str(message.from_user.id)
         regdata = fetchinfo(user_id)
         results = str(regdata)
