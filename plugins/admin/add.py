b575d4fa7ca52f477b60c86653cdf0f6ab8aa8ff
AMAR CHANDAN <78189163+amarchandan@users.noreply.github.com>
2024-05-22 15:12:36 +0530

    Update add.py

--- plugins/admin/add.py
+++ plugins/admin/add.py
@@ -31,3 +31,32 @@ Group (<code>{groupid}</code>) Is Now Authorized ✅.
       """
             await message.reply_text(resp, message.id)
             await message.reply_text(gc, resp)
+
+@Client.on_message(filters.command("add1"))
+async def cmd_ad1d(Client, message):
+    user_id = str(message.from_user.id)
+    CEO = "6697282163"
+    GROUP = open("plugins/group.txt").read().splitlines()
+    if user_id != CEO:
+        resp = "Require Owner Privilages ⚠️"
+        await message.reply_text(resp, message.id)
+    else:
+        chat_add = message.text[len("/add ") :]
+        if len(chat_add) == 0:
+            chat_id = str(message.chat.id)
+        else:
+            chat_id = message.text[len("/add ") :]
+        groupid = chat_id
+        if groupid in GROUP:
+            resp = f"""
+Group (<code>{groupid}</code>) Is Already Authorized ⚠️.
+      """
+            await message.reply_text(resp, message.id)
+        else:
+            with open("plugins/group.txt", "a") as f:
+                f.write(f"{groupid}\n")
+            resp = f"""
+Group (<code>{groupid}</code>) Is Now Authorized ✅.
+      """
+            await message.reply_text(resp, message.id)
+            await message.reply_text(gc, resp)
