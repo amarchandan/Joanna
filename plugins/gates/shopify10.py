df08b361688c30377db614cb4638817df389af59
AMAR CHANDAN <78189163+amarchandan@users.noreply.github.com>
2024-05-26 11:05:22 +0530

    Update shopify10.py

--- plugins/gates/shopify10.py
+++ plugins/gates/shopify10.py
@@ -10,7 +10,7 @@ session = requests.session()
 
 
 @Client.on_message(filters.command("spt"))
-async def cmd_au(Client, message):
+async def cmd_shopify10(Client, message):
     try:
         user_id = str(message.from_user.id)
         chat_type = str(message.chat.type)
