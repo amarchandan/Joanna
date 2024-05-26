815f6bf05ea1870c2655d011f5296bbf204b844f
AMAR CHANDAN <78189163+amarchandan@users.noreply.github.com>
2024-05-26 11:06:54 +0530

    Update and rename strauth.py to stripe_auth.py

--- plugins/gates/stripe_auth.py
+++ plugins/gates/stripe_auth.py
@@ -10,7 +10,7 @@ session = requests.session()
 
 
 @Client.on_message(filters.command("sa"))
-async def cmd_au(Client, message):
+async def cmd_stripeauth(Client, message):
     try:
         user_id = str(message.from_user.id)
         chat_type = str(message.chat.type)
